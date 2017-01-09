d479f35dfe6773ff019547d5c94ce8f501e16d91
David Lu <david.scowluga@gmail.com>
2017-01-08 20:10:49 -0500

    commit

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-var debugmode = true; 
+var debugmode = false; 
 
 var isEasy = getEasy(); 
 var hasScore = getScore(); 
